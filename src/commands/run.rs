de484508717a2f0d77cbdab1fd8c7559650eb111
Lars Riel <larsriel@gmail.com>
2023-11-24 18:15:40 +0000

    fixed healthcheck on app start

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -1,6 +1,10 @@
 use std::thread::sleep;
 
-use crate::{models::Project, utils::networks::Network, APP_STATE};
+use crate::{
+    models::Project,
+    utils::{networks::Network, spinner::Spinner},
+    APP_STATE,
+};
 use run_script::run_script;
 
 use super::nginx::Nginx;
@@ -18,60 +22,56 @@ impl Run {
             Network::nginx_from_project(&project.name).create(),
         );
 
+        Nginx::connect_to_network(&networks.1);
+
+        let mut spinner = Spinner::new();
         for app in &project.apps {
+            spinner.start(format!("{}: ", app.name));
+
             app.run(&vec![&networks.0, &networks.1]);
             let mut up = false;
 
             if app.domains.is_some() {
                 // wait until container is up
-                for seconds in [1, 3, 5] {
+                for seconds in 1..3 {
                     let command = if let Some(port) = &app.port {
                         format!(
-                            "docker exec nbot_nginx curl -Iks http://{}:{}",
+                            "docker exec nbot_nginx curl -Is http://{}:{}",
                             &app.container_name, port
                         )
                     } else {
                         format!(
-                            "docker exec nbot_nginx curl -Iks http://{}",
+                            "docker exec nbot_nginx curl -Is http://{}",
                             &app.container_name
                         )
                     };
 
                     let (code, _, _) = run_script!(command).unwrap_or_default();
 
-                    dbg!(code);
-
-                    if code != 0 {
-                        if seconds != 5 {
-                            sleep(std::time::Duration::from_secs(seconds));
-                        }
-                        continue;
+                    if code == 0 {
+                        up = true;
+                        break;
                     }
 
-                    up = true;
-                    break;
+                    if seconds != 3 {
+                        sleep(std::time::Duration::from_secs(seconds));
+                    }
                 }
             }
 
             if !up {
                 app.stop();
-                eprintln!("Failed to start app {}", app.name);
+                spinner.stop(format!("{}: failed", app.name));
                 continue;
+            } else {
+                spinner.stop(format!("{}: OK", app.name));
             }
 
             Nginx::generate_certificates(app, true);
             Nginx::add_conf(app);
         }
 
-        Nginx::connect_to_network(&networks.1);
-
         let mut app_state = APP_STATE.to_owned();
         app_state.add_or_update_project(project);
     }
 }
-
-// --env MARIADB_USER=example-user
-// --env MARIADB_PASSWORD=my_cool_secret
-// --env MARIADB_DATABASE=exmple-database
-// --env MARIADB_ROOT_PASSWORD=my-secret-pw
-// mariadb:latest
