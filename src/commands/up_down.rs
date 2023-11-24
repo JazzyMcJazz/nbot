b1685cd75693d03f2ab9f1bfd7a52c1382bb0ff9
Lars Riel <larsriel@gmail.com>
2023-11-24 17:56:10 +0000

    fixed nginx crashing on missing upstream

--- src/commands/up_down.rs
+++ src/commands/up_down.rs
@@ -1,6 +1,6 @@
 use crate::{utils::spinner::Spinner, APP_STATE};
 
-use super::nginx::Nginx;
+use super::{nginx::Nginx, run::Run};
 
 pub struct UpDown;
 
@@ -17,13 +17,17 @@ impl UpDown {
         }
 
         for project in state.projects {
-            for app in project.apps {
-                if !app.is_running() {
-                    spinner.start(format!("{}: ", app.name));
-                    app.run(&vec![]);
-                    spinner.stop(format!("{}: OK", app.name));
-                }
-            }
+            // let project_name = project.name.clone();
+            // spinner.start(format!("{}: ", &project_name));
+            Run::project(project); // TODO: error handling
+                                   // spinner.stop(format!("{}: OK", project_name));
+                                   // for app in project.apps {
+                                   //     if !app.is_running() {
+                                   //         spinner.start(format!("{}: ", app.name));
+                                   //         app.run(&vec![]);
+                                   //         spinner.stop(format!("{}: OK", app.name));
+                                   //     }
+                                   // }
         }
     }
 
--- src/files/nginx_entrypoint.sh
+++ src/files/nginx_entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-echo "Certbot entrypoint is running."
-
-mv default.conf /etc/nginx/conf.d/default >> /dev/null 2>&1
-
-crontab scheduler.txt
-crontab -l
-
 function inotifywait_listen() {
     while inotifywait -e create -e modify -e delete -e move /etc/nginx/conf.d; do
         echo "Configuration change detected, reloading Nginx..."
         nginx -s reload
     done
 }
+
+echo "Certbot entrypoint is running."
+
+cp default.conf /etc/nginx/conf.d/default >> /dev/null 2>&1
+
+crontab scheduler.txt
+crontab -l
 
 # Monitor the conf.d directory for changes and reload Nginx when a change is detected
 inotifywait_listen &
